9c8c015e44e0fcad2bfaddf35086f86710bcd7b3
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-10 07:09:25 +0000

    conversion of datatype

---
+++ start/conversionOperation.js
@@ -0,0 +1,35 @@
+// let score=null // 0 output
+// let score=undefined//NaN output
+//    let score= true // 1 and 0->false
+   let score= "abbisj"//NaN 
+
+
+    console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+// "33"->33
+// "33abc"->NaN
+//true->1; false->0
+
+// let isloggedIn=1
+ let isloggedIn=" anh"
+
+
+let booleanIsLoggedIn= Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1->true; 0->false
+//""->false
+//"name"->true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
