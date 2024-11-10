69879f299a160f6195af42319fd93088c1635815
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-10 06:44:24 +0000

    datatypes

---
+++ start/datatype.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+let name="abi"
+let age=18
+let isloggedIn= false
+let state;
+
+// primitive datatype
+
+// number -> 2^53
+// bigint
+//string->""
+//boolean->true/false
+//null ->standalone value
+//undefined->
+//symbol->uniqueness
+
+// objects
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
+
+
+
