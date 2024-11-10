78902fc3abc21e8b6533c0488cc08b2eff6b5871
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-10 08:22:12 +0000

    comparison

---
+++ start/comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+                              // avoid to write this type of code 
+                              // try to write clean code
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+
+// strict check(===) 
+console.log("2"===2);
+
+
+
+
