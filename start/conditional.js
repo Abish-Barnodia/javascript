7032e52637d72e2b08452499e168eb8bef231413
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-16 18:17:53 +0000

    conditional

---
+++ start/conditional.js
@@ -0,0 +1,30 @@
+// conditional
+ let age =0;
+if(age!==18){
+    console.log("you can drive");
+    
+
+}
+
+else if(age==0)
+{
+    console.log("are you kidding");
+    
+}
+
+else {
+    console.log("cannot drive");
+    
+}
+a=6;
+b=8;
+let c = a>b ? (a-b):(b-a)
+/*
+translate to:
+if(a>b){
+ let c=a-b;
+}
+ else{
+  let c=a-b;
+ }
+  */
