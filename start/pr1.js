f6686ee9956680a9d4662472668538f5c788eae6
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-16 17:23:11 +0000

    pratice1

---
+++ start/pr1.js
@@ -0,0 +1,25 @@
+// create a variable of type string  and try to add no. on it
+
+let x="jklf";
+console.log(x+1);
+
+// datatype of prev questuion
+console.log(typeof x);
+
+const a=5
+console.log(a);
+ c=a+5;
+ console.log(c);
+ 
+
+ 
+// create a dictinory of 3 words
+
+var d={};
+
+d["youtube"]="video";
+d["whatsapp"]="chat";
+d["steamit"]="web3";
+
+console.log(d);
+
