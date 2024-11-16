c7dd14c0b2db838cea148199b6640acb4ea5c9a8
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-16 19:31:28 +0000

    loops

---
+++ start/function.js
@@ -0,0 +1,28 @@
+console.log("start with function");
+
+function nice(name) {
+    console.log("he" + name + "y you are nice");
+    console.log("he" + name + "y you are great");
+    console.log("he" + name + "y you are good");
+    console.log("he" + name + "y you are damm good");
+
+}
+
+
+
+function sum(a, b) {
+    console.log(a + b)
+    return a + b
+}
+// result = sum(3, 5)
+
+// console.log("the sum is: ", result);
+
+
+const func1= (x)=>{
+    console.log("i am arrow function",x);
+    
+}
+
+func1(34);
+func1(66);
