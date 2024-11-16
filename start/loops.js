e85f9235043643c0c1a8ee3198e5bf4ec02b8787
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-16 19:11:10 +0000

    loops

---
+++ start/loops.js
@@ -0,0 +1,41 @@
+console.log("start with loops");
+let a=1;
+
+// for loop:->
+
+// for (let i = 0; i <10; i++) {
+//         console.log(a+i);  
+// }
+
+// let obj={
+//     name:"Harry",
+//     role: "programmer",
+//     company: "code with Ai"
+// }
+// for (const key in obj) {
+//         const element = obj[key];
+//        console.log(element);
+// }
+
+// for (const c of "Harry") {
+//     console.log(c);
+    
+// }
+
+
+// while loop
+
+// let i=0;
+// while(i<6){
+//     console.log(i);
+//     i++;
+    
+// }
+
+//do while
+
+let i=10;
+do{
+console.log(i)
+i++;
+}while(i<6);
