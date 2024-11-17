e3706c89d6e729dcb75295f6b372860cb857464a
Abish-Barnodia <abishbarnodia2018@gmail.com>
2024-11-17 21:09:46 +0000

    string

---
+++ start/string.js
@@ -0,0 +1,19 @@
+console.log("this is string container")
+let a="ab"
+console.log(a.length)
+
+let real_name="harry"
+let friend ="rohan"
+console.log("his "+real_name+friend);
+console.log(`real name ${real_name}and ${friend}`);
+
+
+let b="shivam"
+console.log(b.toLocaleUpperCase());
+console.log(b.toLocaleLowerCase());
+
+console.log(b.slice(0,5));
+console.log(b.replace("sh",77));
+
+console.log(b.concat(a,"ash"));
+
