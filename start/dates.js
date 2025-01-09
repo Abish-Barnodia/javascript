72fce4db9eb20b45902ac21ec4385d1ea0333423
Abish-Barnodia <abishbarnodia2018@gmail.com>
2025-01-09 18:57:49 +0000

    dates

--- start/dates.js
+++ start/dates.js
@@ -6,7 +6,7 @@ console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleString()); 
 console.log(myDate.toLocaleTimeString());
 console.log(myDate.toTimeString());
 console.log(myDate.toUTCString());
