791a6f927bb538467a63ea6dd7f46f896d09cf2f
Abish-Barnodia <abishbarnodia2018@gmail.com>
2025-01-10 06:45:49 +0000

    object3.js

---
+++ start/obj2.js
@@ -0,0 +1,38 @@
+
+// objects destructuring
+
+const course={
+    coursename :"javascript",
+    price:"999",
+    courseInstructor:"abish"
+}
+
+const {courseInstructor: instructor} =course // destructing kiya courseInstructor ko
+
+// console.log(courseInstructor);
+
+console.log(instructor);
+
+// const navbar =({company}) => {
+
+// }                   // destructuring in react
+// navbar(company= "abish")
+
+// Api's -> apna kam dusro ka sir pr dalna 
+
+  // Api's ->objects  ki form ma
+
+// {
+//   "  name":"abish",
+//    " cousename ": "javascript",
+//   "  price": "free"
+// }
+
+
+// Api's ->arrays ki form ma
+
+[
+    {},
+    {},
+    {}
+]
