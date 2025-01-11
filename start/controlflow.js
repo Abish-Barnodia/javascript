02fcc5c19fa3b9a70cbe0b3f26fa869ae0fcfb09
Abish-Barnodia <abishbarnodia2018@gmail.com>
2025-01-11 10:40:52 +0000

    controlflow1.js

---
+++ start/controlflow.js
@@ -0,0 +1,69 @@
+// control flow
+
+
+//if
+ 
+// const isUserLoggedIn=true
+
+if (true){
+// ander ka code execcute hoga 
+
+}
+
+if (false){
+// ander ka code execute nhi hoga
+}
+ const temperature=41
+if(temperature<50){
+console.log("less than 50");
+
+}else{
+console.log("greater than 50");
+}
+
+// <,>,<=,>=,==,!=,=== ,!==
+
+
+// const score =200
+// if(score>100){
+// let power ="fly"
+//     console.log(`user power: ${power}`);
+// }
+   
+// console.log(`user power: ${power}`);
+
+
+// const balance =1000
+// if (balance>500) console.log("test");
+
+// if(balance<500){
+//     console.log("less than");
+    
+// }else if(balance<750){
+
+// console.log("less than 750");
+// }
+// else if(balance<200)
+// {
+//     console.log("less than 900");
+    
+// }
+// else{
+//     console.log("less than 1000");
+    
+// }
+
+const isUserLoggedIn = true
+const debitCard= true
+
+const loggedInFromGoogle=false
+const loggedInFromEmail = true
+
+if(isUserLoggedIn&&debitCard&&2==2){
+    console.log("Allow to buy couse");
+    
+}
+
+if(loggedInFromGoogle||loggedInFromEmail){
+    console.log("user logged in");
+}
